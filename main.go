daa8d6000555d6d7302f37039dbcd10507376a96
Phillip Ahereza <pahereza@icloud.com>
2024-12-13 15:26:11 +0100

    Refactor: Improve readability
    - Add better logging

--- main.go
+++ main.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"context"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
 
+	"github.com/cespare/xxhash/v2"
 	"github.com/libp2p/go-libp2p"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/libp2p/go-libp2p/core/host"
@@ -21,6 +25,7 @@ import (
 	"github.com/urfave/cli/v2"
 	"github.com/vmihailenco/msgpack/v5"
 	"golang.design/x/clipboard"
+	"golang.org/x/sync/singleflight"
 )
 
 const (
@@ -46,7 +51,77 @@ type discoverer struct {
 	h host.Host
 }
 
+// messageCache is a thread-safe cache for storing message hashes
+type messageCache struct {
+	sync.RWMutex
+	cache map[string]struct{}
+}
+
+// config struct for omniclip settings
+type config struct {
+	TopicName   string
+	ServicePort int
+	StatusPort  int
+	LogLevel    string
+}
+
+func hashContent(content string) string {
+	hasher := xxhash.New()
+	hasher.WriteString(content)
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+// cleaner periodically clears the message cache to prevent it from growing indefinitely.
+func (mc *messageCache) cleaner(ctx context.Context) {
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			mc.clearCache()
+		}
+	}
+
+}
+
+func (mc *messageCache) addEntry(entry string) bool {
+	key := hashContent(entry)
+	mc.Lock()
+	defer mc.Unlock()
+
+	if _, ok := mc.cache[key]; ok {
+		return false
+	}
+	mc.cache[key] = struct{}{}
+	return true
+}
+
+func (mc *messageCache) contains(entry string) bool {
+	key := hashContent(entry)
+	mc.RLock()
+	defer mc.RUnlock()
+
+	_, ok := mc.cache[key]
+	return ok
+}
+
+func (mc *messageCache) clearCache() {
+	mc.Lock()
+	defer mc.Unlock()
+	mc.cache = make(map[string]struct{})
+}
+
+func newMessageCache(ctx context.Context) *messageCache {
+	mc := &messageCache{cache: make(map[string]struct{})}
+	go mc.cleaner(ctx)
+	return mc
+}
+
 func main() {
+	var loglevel string
 	app := &cli.App{
 		Name:        "omniClip",
 		Usage:       "A cross-platform clipboard sharing tool.",
@@ -57,7 +132,7 @@ func main() {
 				Aliases: []string{"s"},
 				Usage:   "Check the status of the omniclip node.",
 				Action: func(c *cli.Context) error {
-					return getStatus(defaultStatusPort)
+					return getStatus(&config{StatusPort: defaultStatusPort})
 				},
 			},
 			{
@@ -83,29 +158,44 @@ func main() {
 						Value:   defaultStatusPort,
 						Usage:   "Port for the status server.",
 					},
+					&cli.StringFlag{
+						Name:        "loglevel",
+						Aliases:     []string{"l"},
+						Usage:       "Set the logging level (DEBUG, INFO, ERROR, WARN).",
+						Destination: &loglevel,
+						EnvVars:     []string{"OMNICLIP_LOGLEVEL"},
+						Value:       "ERROR",
+					},
 				},
 				Action: func(c *cli.Context) error {
-					return startNode(c.Context, c.Int("port"), c.Int("status"), c.String("topic"))
+					cfg := &config{
+						TopicName:   c.String("topic"),
+						ServicePort: c.Int("port"),
+						StatusPort:  c.Int("status"),
+						LogLevel:    c.String("loglevel"),
+					}
+					return startNode(c.Context, cfg)
+				},
+				Before: func(c *cli.Context) error {
+					var handler slog.Handler
+					switch loglevel {
+					case "DEBUG":
+						handler = slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelDebug})
+					case "INFO":
+						handler = slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelInfo})
+					case "WARN":
+						handler = slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelWarn})
+					case "ERROR":
+						handler = slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelError})
+					default:
+						handler = slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelError})
+					}
+					slog.SetDefault(slog.New(handler))
+					return nil
 				},
 			},
 		},
 		ErrWriter: io.Discard,
-
-		Version: "1.0.0",
-		Flags: []cli.Flag{
-			&cli.BoolFlag{
-				Name:    "version",
-				Aliases: []string{"v"},
-				Usage:   "Print the version and exit.",
-			},
-		},
-		Before: func(c *cli.Context) error {
-			if c.Bool("version") {
-				fmt.Println(c.App.Version)
-				os.Exit(0)
-			}
-			return nil
-		},
 	}
 
 	err := app.Run(os.Args)
@@ -115,9 +205,9 @@ func main() {
 }
 
 // getStatus retrieves and prints the status of the omniclip node.
-func getStatus(port int) error {
+func getStatus(cfg *config) error {
 	client := http.Client{Timeout: 1 * time.Second}
-	resp, err := client.Get(fmt.Sprintf("http://0.0.0.0:%d/status", port))
+	resp, err := client.Get(fmt.Sprintf("http://0.0.0.0:%d/status", cfg.StatusPort))
 	if err != nil {
 		fmt.Println("node status: 🛑\n\nis omniClip running?")
 		return fmt.Errorf("connecting to status server: %w", err)
@@ -145,7 +235,8 @@ func getStatus(port int) error {
 	return nil
 }
 
-func initServer(port int, h host.Host, tp *pubsub.Topic) *http.Server {
+func initServer(ctx context.Context, cfg *config, tp *pubsub.Topic, h host.Host) *http.Server {
+	logger := slog.Default()
 	mux := http.NewServeMux()
 	mux.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
 		peerIds := tp.ListPeers()
@@ -159,26 +250,40 @@ func initServer(port int, h host.Host, tp *pubsub.Topic) *http.Server {
 			Peers: peers,
 		}
 		if err := json.NewEncoder(w).Encode(status); err != nil {
-			log.Println("Error encoding status response:", err)
+			logger.Error("Error encoding status response", "error", err)
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		}
 	})
 
 	server := &http.Server{
-		Addr:              fmt.Sprintf(":%d", port),
+		Addr:              fmt.Sprintf(":%d", cfg.StatusPort),
 		Handler:           mux,
 		ReadHeaderTimeout: 1 * time.Second,
 	}
 
+	go func() {
+		<-ctx.Done()
+		slog.Info("Shutting down status server")
+
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(shutdownCtx); err != nil {
+			logger.Error("Error shutting down status server", "error", err)
+		}
+	}()
+
 	return server
 }
 
-func startNode(ctx context.Context, p2pPort, statusPort int, topicName string) error {
+func startNode(ctx context.Context, cfg *config) error {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
+	logger := slog.Default()
+
 	// Create libp2p host
-	h, err := libp2p.New(libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", p2pPort)))
+	h, err := libp2p.New(libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", cfg.ServicePort)))
 	if err != nil {
 		return fmt.Errorf("error creating libp2p host: %w", err)
 	}
@@ -193,7 +298,7 @@ func startNode(ctx context.Context, p2pPort, statusPort int, topicName string) e
 		return fmt.Errorf("error creating pubsub service: %w", err)
 	}
 
-	topic, err := ps.Join(topicName)
+	topic, err := ps.Join(cfg.TopicName)
 	if err != nil {
 		return fmt.Errorf("error joining pubsub topic: %w", err)
 	}
@@ -204,22 +309,25 @@ func startNode(ctx context.Context, p2pPort, statusPort int, topicName string) e
 	}
 
 	// Start status server
-	server := initServer(statusPort, h, topic)
-	errChan := make(chan error)
+	server := initServer(ctx, cfg, topic, h)
+	errChan := make(chan error, 1)
 
 	go func() {
+		logger.Info("Starting status server", "port", cfg.StatusPort)
 		errChan <- server.ListenAndServe()
 	}()
 
 	// Start peer discovery
-	go func() {
-		errChan <- setupDNSDiscovery(h)
-	}()
+	if err = setupDNSDiscovery(h); err != nil {
+		return fmt.Errorf("error setting up DNS discovery: %w", err)
+	}
 
 	// Start Clipboard Watcher and Message Receiver
-	go watchClipboard(ctx, topic, clipboard.Watch(ctx, clipboard.FmtText), h.ID())
+	cache := newMessageCache(ctx)
+	sf := singleflight.Group{}
+	go watchClipboard(ctx, topic, clipboard.Watch(ctx, clipboard.FmtText), h.ID(), cache, &sf)
 
-	go receiveMessages(ctx, subscription, h.ID())
+	go receiveMessages(ctx, subscription, h.ID(), cache, &sf)
 
 	// Handle graceful shutdown
 	stop := make(chan os.Signal, 1)
@@ -227,14 +335,13 @@ func startNode(ctx context.Context, p2pPort, statusPort int, topicName string) e
 
 	select {
 	case <-stop:
-		timeoutCtx, svCancel := context.WithTimeout(ctx, 3*time.Second)
-		defer svCancel()
-		if err := server.Shutdown(timeoutCtx); err != nil {
-			return fmt.Errorf("server shutdown: %w", err)
-		}
+		logger.Info("Shutting down...")
+		cancel()
 		return nil
 
 	case svErr := <-errChan:
+		logger.Error("Server error", "error", svErr)
+		cancel()
 		return svErr
 	}
 }
@@ -242,67 +349,98 @@ func startNode(ctx context.Context, p2pPort, statusPort int, topicName string) e
 // setupDNSDiscovery sets up mDNS discovery for the libp2p host.
 func setupDNSDiscovery(h host.Host) error {
 	s := mdns.NewMdnsService(h, serviceName, &discoverer{h: h})
-	if err := s.Start(); err != nil {
-		return fmt.Errorf("failed to start mdns service: %w", err)
-	}
-	return nil
+	return s.Start()
 }
 
 // HandlePeerFound is called when a new peer is discovered via mDNS.
 // It attempts to connect to the peer if it's not the host itself.
 func (n *discoverer) HandlePeerFound(pi peer.AddrInfo) {
+	logger := slog.Default()
+
 	if pi.ID == n.h.ID() {
 		return
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-
-	log.Printf("Discovered peer: %s\n", pi.ID.ShortString())
+	logger.Info("Discovered peer", "peerID", pi.ID.ShortString())
 
 	err := n.h.Connect(ctx, pi)
 	if err != nil {
-		log.Printf("error connecting to peer %s: %s\n", pi.ID.ShortString(), err)
+		logger.Warn("error connecting to peer", "peerID", pi.ID.ShortString(), "error", err)
 	}
 }
 
-// watchClipboard monitors the clipboard for changes and publishes new entries to the pubsub topic.
-func watchClipboard(ctx context.Context, topic *pubsub.Topic, clipboardCh <-chan []byte, id peer.ID) {
+// watchClipboard monitors the clipboard for changes and publishes new entries to the pubSub topic.
+func watchClipboard(ctx context.Context, topic *pubsub.Topic, clipboardCh <-chan []byte, id peer.ID, mc *messageCache, sf *singleflight.Group) {
+	logger := slog.Default()
 	for msg := range clipboardCh {
-		e := &entry{Value: string(msg), Source: id}
-		data, err := msgpack.Marshal(e)
+		// Use singleflight to debounce clipboard updates
+		_, err, _ := sf.Do("clipboardUpdate", func() (interface{}, error) {
+			// check if the content has already been seen. If so, skip publishing
+			logger.Info("Clipboard updated", "text", string(msg))
+			e := &entry{Value: string(msg), Source: id}
+
+			if !mc.addEntry(e.Value) {
+				return nil, nil // Skip publishing if already in cache
+			}
+
+			data, err := msgpack.Marshal(e)
+			if err != nil {
+				return nil, fmt.Errorf("marshalling clipboard entry: %w", err)
+			}
+
+			// Publish the serialized entry to the pubSub topic
+			if err := topic.Publish(ctx, data); err != nil {
+				return nil, fmt.Errorf("publishing clipboard entry: %w", err)
+			}
+			return nil, nil
+		})
+
 		if err != nil {
-			log.Println("Error marshalling clipboard entry", err)
-			continue
+			logger.Warn("Error in clipboard update:", "error", err)
 		}
 
-		// Publish the serialized entry to the pubsub topic
-		if err := topic.Publish(ctx, data); err != nil {
-			log.Println(err)
-		}
 	}
 }
 
-// receiveMessages receives messages from the pubsub topic, unmarshals them,
+// receiveMessages receives messages from the pubSub topic, unmarshals them,
 // and updates the local clipboard if the message originated from a different peer.
-func receiveMessages(ctx context.Context, sub *pubsub.Subscription, id peer.ID) {
+func receiveMessages(ctx context.Context, sub *pubsub.Subscription, id peer.ID, mc *messageCache, sf *singleflight.Group) {
+	logger := slog.Default()
 	for {
 		m, err := sub.Next(ctx)
 		if err != nil {
 			if errors.Is(err, context.Canceled) {
 				return
 			}
-			log.Println("Error receiving message:", err)
+			logger.Warn("Error receiving message", "error", err)
 			continue
 		}
 
 		var e entry
 		err = msgpack.Unmarshal(m.Data, &e)
 		if err != nil {
-			log.Println("Error unmarshalling message: ", err)
+			logger.Warn("Error unmarshalling message", "error", err)
 			continue
 		}
-		if e.Source != id {
-			clipboard.Write(clipboard.FmtText, []byte(e.Value))
+
+		if e.Source == id { // ignore messages that originated from the current node
+			continue
+		}
+
+		if mc.contains(e.Value) { // ignore messages that have already been seen
+			continue
+		}
+
+		_, err, _ = sf.Do("clipboardWrite", func() (interface{}, error) {
+			if e.Source != id {
+				clipboard.Write(clipboard.FmtText, []byte(e.Value))
+			}
+			return nil, nil
+		})
+
+		if err != nil {
+			logger.Warn("Error writing to clipboard:", "error", err)
 		}
 	}
 }
